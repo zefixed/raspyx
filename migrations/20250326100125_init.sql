a0117b739a4a8c541363f72bb530a613d001375b
zefixed <gerasenkov.code@mail.ru>
2025-03-26 15:48:42 +0300

    Replaced id with uuid in migration

--- migrations/20250326100125_init.sql
+++ migrations/20250326100125_init.sql
@@ -1,45 +1,45 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS teachers (
-    id UUID PRIMARY KEY,
+    uuid UUID PRIMARY KEY,
     first_name VARCHAR NOT NULL,
     second_name VARCHAR NOT NULL,
     middle_name VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS groups (
-    id UUID PRIMARY KEY,
+    uuid UUID PRIMARY KEY,
     number VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS rooms (
-    id UUID PRIMARY KEY,
+    uuid UUID PRIMARY KEY,
     number VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS subjects (
-    id UUID PRIMARY KEY,
+    uuid UUID PRIMARY KEY,
     name VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS subj_types (
-    id UUID PRIMARY KEY,
+    uuid UUID PRIMARY KEY,
     type VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS locations (
-    id UUID PRIMARY KEY,
+    uuid UUID PRIMARY KEY,
     name VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS schedule (
-    id UUID PRIMARY KEY,
-    teacher_id UUID REFERENCES teachers(id),
-    group_id UUID NOT NULL REFERENCES groups(id),
-    room_id UUID REFERENCES rooms(id),
-    subject_id UUID NOT NULL REFERENCES subjects(id),
-    type_id UUID NOT NULL REFERENCES subj_types(id),
-    location_id UUID NOT NULL REFERENCES locations(id),
+    uuid UUID PRIMARY KEY,
+    teacher_uuid UUID REFERENCES teachers(uuid),
+    group_uuid UUID NOT NULL REFERENCES groups(uuid),
+    room_uuid UUID REFERENCES rooms(uuid),
+    subject_uuid UUID NOT NULL REFERENCES subjects(uuid),
+    type_uuid UUID NOT NULL REFERENCES subj_types(uuid),
+    location_uuid UUID NOT NULL REFERENCES locations(uuid),
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
     start_date DATE NOT NULL,
@@ -48,9 +48,9 @@ CREATE TABLE IF NOT EXISTS schedule (
     link TEXT
 );
 
-CREATE INDEX IF NOT EXISTS idx_schedule_teacher_id ON schedule(teacher_id);
-CREATE INDEX IF NOT EXISTS idx_schedule_group_id ON schedule(group_id);
-CREATE INDEX IF NOT EXISTS idx_schedule_room_id ON schedule(room_id);
+CREATE INDEX IF NOT EXISTS idx_schedule_teacher_uuid ON schedule(teacher_uuid);
+CREATE INDEX IF NOT EXISTS idx_schedule_group_uuid ON schedule(group_uuid);
+CREATE INDEX IF NOT EXISTS idx_schedule_room_uuid ON schedule(room_uuid);
 -- +goose StatementEnd
 
 -- +goose Down
