6f8b7575d253368ef582a4bc8351db48dd617fba
Gerasenkov Ilya <71497957+zefixed@users.noreply.github.com>
2025-04-01 16:57:23 +0300

    Added examples for models
    * Added examples for models
    
    * Added examples for models

--- internal/domain/models/teacher.go
+++ internal/domain/models/teacher.go
@@ -3,8 +3,8 @@ package models
 import "github.com/google/uuid"
 
 type Teacher struct {
-	UUID       uuid.UUID `json:"uuid"`
-	FirstName  string    `json:"first_name"`
-	SecondName string    `json:"second_name"`
-	MiddleName string    `json:"middle_name,omitempty"`
+	UUID       uuid.UUID `json:"uuid" example:"c555b9e8-0d7a-11f0-adcd-20114d2008d9"`
+	FirstName  string    `json:"first_name" example:"Имя"`
+	SecondName string    `json:"second_name" example:"Фамилия"`
+	MiddleName string    `json:"middle_name,omitempty" example:"Отчество"`
 }
